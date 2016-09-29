33c247f2c3fa541e26dcbb6ebb3a8317c6bca6a8
Carlos <c_aag@icloud.com>
2016-09-28 22:06:11 -0500

    juanelcallado

--- laboratorios/laboratorios1/juanelcallado/juanelcallado.py
+++ laboratorios/laboratorios1/juanelcallado/juanelcallado.py
@@ -1,12 +1,11 @@
-preg = input("Preguntale a Juan:")
+while True:
+    preg = input("Preguntale a Juan:")
 
-if preg.endswith('?'):
-    print ("Ofi")
-
-elif preg >= 'A' and preg <= 'Z':
-    print ("Chillea")
-
-elif (len(preg)==0):
-    print ("Mmm")
-else:
-    print ("Me da igual")
+    if preg.endswith('?'):
+        print ("Ofi")
+    elif preg >= 'A' and preg <= 'Z':
+        print ("Chillea")
+    elif (len(preg)==0):
+        print ("Mmm")
+    else:
+        print ("Me da igual")
